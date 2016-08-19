e3c577a35c6e58ad0bc57e65ea9fe9f788e7082f
R4nd0m6uy <r4nd0m6uy@r4nd0m6uy.ch>
2016-08-19 19:45:56 +0200

    Initial commit

---
+++ content/HelloWorld.md
@@ -0,0 +1,11 @@
+Title: printf("Hello World\n");
+Date: 2016-08-19 19:00
+Category: News
+Tags: none
+
+This is my brand new blog where I want to write and speak about nerdy stuff such
+as video games, technology, let's say 
+[GNU/Linux](https://en.wikipedia.org/wiki/GNU/Linux_naming_controversy) 
+softwares more precisly and life in general.
+
+I hope you will enjoy it, happy surfing!
---
+++ pelicanconf.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'R4nd0m 6uy'
+SITENAME = "R4nd0m's Blog"
+SITEURL = 'http://www.r4nd0m6uy.ch'
+FAVICON = SITEURL + '/images/favicon.ico'
+PATH = 'content'
+TIMEZONE = 'Europe/Zurich'
+DEFAULT_LANG = 'en'
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+DEFAULT_PAGINATION = 5
+STATIC_PATHS = ['images']
+LINKS = (
+        )
+SOCIAL =  (
+            ('rss', '//r4nd0m6uy.ch/feeds/all.atom.xml'),
+            ('github', 'https://github.com/r4nd0m6uy'),
+            ('linkedin', 'https://www.linkedin.com/in/guy-morand-6baa7223'),
+            ('soundcloud', 'https://soundcloud.com/random-guy'),
+          )
+#RELATIVE_URLS = True
+
+# Flex template configuration
+THEME = u'../flex'
+SITETITLE = 'R4nd0m\'s'
+SITESUBTITLE = 'Software Engineer'
+SITEDESCRIPTION = 'Description'
+CC_LICENSE = True
+COPYRIGHT_YEAR = 2016
+SITELOGO = u'//en.gravatar.com/userimage/31970073/6fb72a3845a6aea9964b00bd864c29dc.png?size=200'
+MAIN_MENU = True
+MENUITEMS = (
+              ('Archives', '/archives.html'),
+              ('Categories', '/categories.html'),
+              ('Tags', '/tags.html'),
+            )
+
